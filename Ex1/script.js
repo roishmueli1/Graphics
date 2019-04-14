f95765173c18ffe5402b4a0098938edf5492fbb4
roishmueli1 <33421651+roishmueli1@users.noreply.github.com>
2019-04-14 15:25:14 +0300

    update curve

--- Ex1/script.js
+++ Ex1/script.js
@@ -1,12 +1,13 @@
 //https://gist.github.com/rilut/8eb8c76c85f00477329c
 
 let numOfClicks = 0;
-let clickOnLine = false;
-let clickOnCircle = false;
 let x1 = 0;
 let x2 = 0;
 let y1 = 0;
 let y2 = 0;
+
+let x3 = x4 = y3 = y4 = 0 //only for Curve
+let numOfCurveLines = 20;
 
 // -----------------------------------LINE---------------------------------------
 function Line(x1, y1, x2, y2, color){
@@ -34,7 +35,6 @@ function Line(x1, y1, x2, y2, color){
 
 function drawLine() {
 	console.log("drawLine");
-  clickOnLine = true;
   numOfClicks = 0;
   shape = 'drawLine'
 }
@@ -49,14 +49,14 @@ function putPixel(x, y, color) {
 }
 
 // ---------------------------------------CIRCLE-------------------------------
-function myCircle(x1, y1, x2, y2, color) {
+function Circle(x1, y1, x2, y2, color) {
 	var x=0;
 	var a=Math.pow(Math.abs(x1-x2),2)+Math.pow(Math.abs(y1-y2),2);
 	var y=r= Math.sqrt(a);
 
 	var p=3-(2*r);
 	while(x<y) {
-		circ(x1,y1,x,y,color);
+		setPixel(x1,y1,x,y,color);
 		if(p<0)
 			p=p+(4*x)+6;
 		else {
@@ -68,13 +68,16 @@ function myCircle(x1, y1, x2, y2, color) {
 
 }
 
-function circ(x1 ,y1 ,x, y, color) {
+function setPixel(x1 ,y1 ,x, y, color) {
 	putPixel(x1+x, y1+y, color);
 	putPixel(x1-x, y1+y, color);
+
 	putPixel(x1+x, y1-y, color);
 	putPixel(x1-x, y1-y, color);
+
 	putPixel(x1+y, y1+x, color);
 	putPixel(x1-y, y1+x, color);
+
 	putPixel(x1+y, y1-x, color);
 	putPixel(x1-y, y1-x, color);
 }
@@ -82,9 +85,43 @@ function circ(x1 ,y1 ,x, y, color) {
 function drawCircle() {
 	console.log("drawCircle");
 	numOfClicks = 0;
-	clickOnCircle = true;
   shape = 'drawCircle'
 }
+// ---------------------------------------Curve-------------------------------
+function Curve(x1, y1, x2, y2, x3, y3, x4, y4, color) {
+
+	var ax = -x1+(3*x2)-(3*x3)+x4;
+	var bx = (3*x1)-(6*x2)+(3*x3);
+	var cx = -(3*x1)+(3*x2);
+	var dx = x1;
+
+	var ay = -y1+(3*y2)-(3*y3)+y4;
+	var by = (3*y1)-(6*y2)+(3*y3);
+	var cy = -(3*y1)+(3*y2);
+	var dy = y1;
+
+	var xc = x1;
+	var yc = y1;
+
+	for(var t=0.0; t<=1; t=t+(1/numOfCurveLines)) {
+		var x = parseInt((ax*Math.pow(t,3))+(bx*Math.pow(t,2))+(cx*t)+dx);
+		var y = parseInt((ay*Math.pow(t,3))+(by*Math.pow(t,2))+(cy*t)+dy);
+
+		Line(xc, yc, x, y, color);
+
+		xc=x;
+		yc=y;
+	}
+}
+
+function drawCurve() {
+	console.log("drawCurve");
+	linesCurve = $('#numOfLines').val();
+	numOfClicks = 0;
+	shape = 'drawCurve'
+}
+
+// --------------------------------Global varible---------------------------
 let shape = true;
 
 // ------------------------------------MOUSE FUNCTION----------------------
@@ -99,7 +136,7 @@ function onClick(e) {
     	} else if (numOfClicks == 2) {
     			x2 = e.clientX;
     			y2 = e.clientY;
-    			Line(x1, y1, x2, y2, "blue");
+    			Line(x1, y1, x2, y2, "yellow");
     			numOfClicks = 0;
     			x1 = x2 = y1 = y2 = 0;
     	}
@@ -112,10 +149,28 @@ function onClick(e) {
       	} else if (numOfClicks == 2) {
       			x2 = e.clientX;
       			y2 = e.clientY;
-      			myCircle(x1, y1, x2, y2, "blue");
+      			Circle(x1, y1, x2, y2, "green");
       			numOfClicks = 0;
       			x1 = x2 = y1 = y2 = 0;
       	}
         break;
+        case 'drawCurve':
+          numOfClicks += 1;
+      		if(numOfClicks == 1) {
+      			x1 = e.clientX;
+      			y1 = e.clientY;
+      		} else if (numOfClicks == 2) {
+      			x2 = e.clientX;
+      			y2 = e.clientY;
+      		} else if (numOfClicks == 3) {
+      			x3 = e.clientX;
+      			y3 = e.clientY;
+      		} else if (numOfClicks == 4) {
+      			x4 = e.clientX;
+      			y4 = e.clientY;
+      			Curve(x1, y1, x2, y2, x3, y3, x4, y4, "red");
+      			numOfClicks = 0;
+      			x1 = x2 = x3 = x4 = y1 = y2 = y3 = y4 = 0;
+      		}
   }
 }
